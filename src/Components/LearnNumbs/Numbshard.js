c9ee4fa94f898ae8ed8ae50c8922f20253dc0598
Beata12 <beata.marek.94@gmail.com>
2023-10-04 08:12:21 +0200

    numb

--- src/Components/LearnNumbs/Numbshard.js
+++ src/Components/LearnNumbs/Numbshard.js
@@ -1,8 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFaceFrown, faFaceSmile } from "@fortawesome/free-regular-svg-icons";
+import { faHeart, faHeartCrack } from "@fortawesome/free-solid-svg-icons";
 
 function NumbHard() {
-	const numbers = {
+	const [timer, setTimer] = useState(10);
+	const [numbers, setNumbers] = useState([]);
+	const [randomWord, setRandomWord] = useState("");
+	const [correctDigit, setCorrectDigit] = useState("");
+	const [selectedDigit, setSelectedDigit] = useState("");
+	const [correctAnswer, setCorrectAnswer] = useState(null);
+	const [points, setPoints] = useState(0);
+	const [canAnswer, setCanAnswer] = useState(true);
+	const [lives, setLives] = useState(3);
+	const [incorrectAnswers, setIncorrectAnswers] = useState(0);
+	const [gameOver, setGameOver] = useState(false);
+	const [showNextQuestion, setShowNextQuestion] = useState(false);
+
+	const numbersdigit = {
 		1: "jeden",
 		2: "dwa",
 		3: "trzy",
@@ -15,13 +31,6 @@ function NumbHard() {
 		10: "dziesięć",
 	};
 
-	const [randomDigits, setRandomDigits] = useState([]);
-	const [randomDigitWord, setRandomDigitWord] = useState("");
-	const [correctDigit, setCorrectDigit] = useState("");
-	const [selectedDigit, setSelectedDigit] = useState("");
-	const [showFeedback, setShowFeedback] = useState(false);
-	const [isCorrect, setIsCorrect] = useState(false);
-
 	const generateRandomNumbers = () => {
 		const uniqueNumbers = new Set();
 		while (uniqueNumbers.size < 3) {
@@ -30,26 +39,85 @@ function NumbHard() {
 		const optionsArray = [...uniqueNumbers];
 		const randomIndex = Math.floor(Math.random() * optionsArray.length);
 		const correctNum = optionsArray[randomIndex];
-		setRandomDigits(optionsArray);
+		setNumbers(optionsArray);
 		setCorrectDigit(correctNum.toString());
-		setRandomDigitWord(numbers[correctNum]);
+		setRandomWord(numbersdigit[correctNum]);
+	};
+
+	let intervalId; // Declare intervalId outside of useEffect
+
+	const handleDigitClick = (digit) => {
+		if (!canAnswer) return;
+
+		setSelectedDigit(digit);
+		const isCorrect = digit === correctDigit;
+		setCorrectAnswer(isCorrect);
+
+		if (isCorrect) {
+			setPoints((prevPoints) => prevPoints + 1);
+			setCanAnswer(false);
+			clearInterval(intervalId);
+			setTimeout(() => {
+				setCanAnswer(true);
+				setCorrectAnswer(null);
+				generateRandomNumbers();
+				setTimer(10);
+				setShowNextQuestion(true);
+				setTimeout(() => {
+					setShowNextQuestion(false);
+					startTimer();
+				}, 2000);
+			}, 2000);
+		}
+	};
+
+	const generateHeartIcons = () => {
+		const heartIcons = [];
+		for (let i = 0; i < 3 - lives; i++) {
+			heartIcons.push(
+				<FontAwesomeIcon
+					icon={faHeartCrack}
+					className="heart-icon"
+					key={`cracked-heart-${i}`}
+				/>
+			);
+		}
+		for (let i = 0; i < lives; i++) {
+			heartIcons.push(
+				<FontAwesomeIcon
+					icon={faHeart}
+					className="heart-icon"
+					key={`heart-${i}`}
+				/>
+			);
+		}
+		return heartIcons;
+	};
+
+	const startNewGame = () => {
+		setGameOver(false);
+		setPoints(0);
+		setLives(3);
+		setIncorrectAnswers(0);
+		generateRandomNumbers();
 	};
 
 	useEffect(() => {
 		generateRandomNumbers();
-		setShowFeedback(false);
-		setSelectedDigit("");
+		startTimer();
+		return () => {
+			clearInterval(intervalId);
+		};
 	}, []);
 
-	const checkAnswer = () => {
-		setShowFeedback(true);
-		setIsCorrect(selectedDigit === correctDigit);
-	};
-
-	const handleDigitClick = (digit) => {
-		setSelectedDigit(digit);
-		setShowFeedback(false);
-		setIsCorrect(false);
+	const startTimer = () => {
+		intervalId = setInterval(() => {
+			if (timer > 0 && canAnswer) {
+				setTimer((prevTimer) => prevTimer - 1);
+			} else if (timer === 0 && canAnswer) {
+				clearInterval(intervalId);
+			}
+		}, 1000);
 	};
 
 	return (
@@ -58,25 +126,62 @@ function NumbHard() {
 				<div className="d-flex justify-content-center align-items-center">
 					<ul className="text-center">
 						<li className="list-mobile">Poziom trudny</li>
-						<div className="horizontal-options">
-							<p className="digitword">{randomDigitWord}</p>
-						</div>
-						<div className="horizontal-options d-flex justify-content-center">
-							{randomDigits.map((digit, index) => (
-								<div
-									className={`digits mx-2 ${
-										selectedDigit === digit
-											? "selected"
-											: ""
-									}`}
-									key={index}
-									onClick={() => handleDigitClick(digit)}
-								>
-									{digit}
+						{gameOver ? (
+							<div className="gameOver">
+								<div className="list-mobile">KONIEC GRY</div>
+								<div className="list-mobile">
+									Punkty: {points}
 								</div>
-							))}
-						</div>
-
+								<div className="list-mobile">Gratulacje</div>
+								<div className="answer-box-mobile d-flex align-items-center justify-content-center choose-level-mobile">
+									<button
+										onClick={startNewGame}
+										className="btn-mobile"
+									>
+										Zagraj jeszcze raz
+									</button>
+								</div>
+							</div>
+						) : (
+							<>
+								<div className="horizontal-options">
+									<p className="digitword">{randomWord}</p>
+								</div>
+								<div className="container">
+									<div className="row d-flex justify-content-center">
+										{numbers.map((digit, index) => (
+											<div
+												className="col-3 answer-box-mobile d-flex align-items-center justify-content-center equations-mobile"
+												key={index}
+												onClick={() =>
+													handleDigitClick(digit)
+												}
+											>
+												{digit}
+											</div>
+										))}
+									</div>
+								</div>
+								<div className="information-mobile">
+									Czas: {timer}
+								</div>
+								<div className="information-mobile">
+									Punkty: {points}
+								</div>
+								<div className="container">
+									<div className="row">
+										<div className="col">
+											{generateHeartIcons()}
+										</div>
+									</div>
+								</div>
+								{showNextQuestion && (
+									<div className="next-question">
+										Następne pytanie za chwilę...
+									</div>
+								)}
+							</>
+						)}
 						<Link style={{ textDecoration: "none" }} to="/num">
 							<li className="list-mobile">Wybierz inny poziom</li>
 						</Link>
