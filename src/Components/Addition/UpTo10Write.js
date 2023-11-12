d084356126431516d4a39065eb946cfae46a33da
Beata12 <beata.marek.94@gmail.com>
2023-11-12 09:29:56 +0100

    addition - desktop

--- src/Components/Addition/UpTo10Write.js
+++ src/Components/Addition/UpTo10Write.js
@@ -149,6 +149,118 @@ function UpTo10Write() {
 
 	return (
 		<main className="main-dzialy">
+			<div className="dzialy-desktop">
+				<div className="container d-flex justify-content-center align-items-center">
+					<div className="col-8 ">
+						<ul className="text-center">
+							<div className="list-title-desktop">
+								DODAWANIE DO 10
+							</div>
+							{gameOver && (
+								<div className="gameOver">
+									<div className="list-desktop">
+										KONIEC GRY
+									</div>
+									<div className="list-desktop">
+										Punkty: {points}
+									</div>
+									<div className="list-desktop">
+										Gratulacje
+									</div>
+									<div className="list-desktop board-desktop align-items-center justify-content-center">
+										<button
+											onClick={startNewGame}
+											className="btn-desktop"
+										>
+											Zagraj jeszcze raz
+										</button>
+									</div>
+								</div>
+							)}
+							{!gameOver && (
+								<div className="gameOver">
+									<div className="icons-desktop">
+										{showSmile && (
+											<FontAwesomeIcon
+												icon={faFaceSmile}
+												className="smile-icon-desktop"
+											/>
+										)}
+										{showFrown && (
+											<FontAwesomeIcon
+												icon={faFaceFrown}
+												className="frown-icon-desktop"
+											/>
+										)}
+									</div>
+									<div>
+										<div className="container">
+											<div className="row d-flex justify-content-center">
+												<div className="col-2 equations-desktop">
+													{number1}
+												</div>
+												<div className="col-2 equations-desktop">
+													+
+												</div>
+												<div className="col-2 equations-desktop">
+													{number2}
+												</div>
+											</div>
+										</div>
+										<div className="container">
+											<div className="row d-flex justify-content-center">
+												<input
+													className=" answer-box-desktop d-flex align-items-center justify-content-center input-mobile input-bck-mobile"
+													type="number"
+													value={userInput}
+													onChange={handleUserInput}
+													max="10"
+													disabled={isCheckingAnswer} // Disable input while checking answer
+												/>
+												<button
+													className=" answer-box-desktop d-flex align-items-center justify-content-center input-mobile"
+													onClick={() => {
+														checkAnswer();
+													}}
+													disabled={
+														!canAnswer ||
+														isCheckingAnswer
+													} // Disable button when answering is disabled or answer is being checked
+												>
+													Sprawdź
+												</button>
+											</div>
+										</div>
+										<div className="information-desktop">
+											Czas: {timer}
+										</div>
+										<div className="information-desktop">
+											Punkty: {points}
+										</div>
+										<div className="container">
+											<div className="row">
+												<div className="col">
+													{generateHeartIcons()}
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							)}
+							<Link style={{ textDecoration: "none" }} to="/add">
+								<li className="list-desktop board-desktop align-items-center justify-content-center">
+									Wybierz inny poziom
+								</li>
+							</Link>
+							<Link style={{ textDecoration: "none" }} to="/">
+								<li className="list-desktop board-desktop align-items-center justify-content-center">
+									Powrót do menu
+								</li>
+							</Link>
+						</ul>
+					</div>
+				</div>
+			</div>
 			<div className="dzialy-mobile">
 				<div className="d-flex justify-content-center align-items-center">
 					<ul className="text-center">
