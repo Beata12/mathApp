fcb1ff87728b5bb1eaf6e879ee8fd45dbaa05368
Beata12 <beata.marek.94@gmail.com>
2023-11-25 14:38:22 +0100

    nav

--- src/Components/Dzialy.js
+++ src/Components/Dzialy.js
@@ -84,7 +84,7 @@ function Dzialy() {
 			</div>
 			<div className="dzialy-mobile">
 				<div className="d-flex justify-content-center align-items-center">
-					<ul className="text-center">
+					<ul className="text-center main-mobile">
 						<Link style={{ textDecoration: "none" }} to="./num">
 							<li className="answer-box-mobile d-flex align-items-center justify-content-center choose-level-mobile">
 								Poznajemy liczby
@@ -124,99 +124,3 @@ function Dzialy() {
 }
 
 export default Dzialy;
-
-// import React from "react";
-// import { Link } from "react-router-dom";
-
-// function Dzialy() {
-// 	return (
-// 		<main className="main-dzialy">
-// 			<div className="dzialy-desktop">
-// 				<div className="container d-flex justify-content-center align-items-center">
-// 					<div className="col-8 ">
-// 						<ul className="text-center">
-// 							<li className="list-desktop board-desktop main-title">
-// 								DZIAŁY
-// 							</li>
-// 							<Link style={{ textDecoration: "none" }} to="./num">
-// 								<li className="list-desktop board-desktop hover-menu">
-// 									Poznajemy liczby
-// 								</li>
-// 							</Link>
-// 							<Link
-// 								style={{ textDecoration: "none" }}
-// 								to="./sign"
-// 							>
-// 								<li className="list-desktop board-desktop hover-menu">
-// 									Poznajemy znaki matematyczne
-// 								</li>
-// 							</Link>
-// 							<Link style={{ textDecoration: "none" }} to="./add">
-// 								<li className="list-desktop board-desktop hover-menu">
-// 									Uczymy się dodawać
-// 								</li>
-// 							</Link>{" "}
-// 							<Link style={{ textDecoration: "none" }} to="./sub">
-// 								<li className="list-desktop board-desktop hover-menu">
-// 									Uczymy się odejmować
-// 								</li>
-// 							</Link>{" "}
-// 							<Link
-// 								style={{ textDecoration: "none" }}
-// 								to="./comp"
-// 							>
-// 								<li className="list-desktop board-desktop hover-menu">
-// 									Porównywanie liczb
-// 								</li>
-// 							</Link>
-// 							<Link style={{ textDecoration: "none" }} to="./un">
-// 								<li className="list-desktop board-desktop hover-menu">
-// 									Działania z niewiadomą
-// 								</li>
-// 							</Link>
-// 						</ul>
-// 					</div>
-// 				</div>
-// 			</div>
-// 			<div className="dzialy-mobile">
-// 				<div className="d-flex justify-content-center align-items-center">
-// 					<ul className="text-center">
-// 						<Link style={{ textDecoration: "none" }} to="./num">
-// 							<li className="answer-box-mobile d-flex align-items-center justify-content-center choose-level-mobile">
-// 								Poznajemy liczby
-// 							</li>
-// 						</Link>
-// 						<Link style={{ textDecoration: "none" }} to="./sign">
-// 							<li className="answer-box-mobile d-flex align-items-center justify-content-center choose-level-mobile">
-// 								Poznajemy znaki matematyczne
-// 							</li>
-// 						</Link>
-
-// 						<Link style={{ textDecoration: "none" }} to="./add">
-// 							<li className="answer-box-mobile d-flex align-items-center justify-content-center choose-level-mobile">
-// 								Uczymy się dodawać
-// 							</li>
-// 						</Link>
-// 						<Link style={{ textDecoration: "none" }} to="./sub">
-// 							<li className="answer-box-mobile d-flex align-items-center justify-content-center choose-level-mobile">
-// 								Uczymy się odejmować
-// 							</li>
-// 						</Link>
-// 						<Link style={{ textDecoration: "none" }} to="./comp">
-// 							<li className="answer-box-mobile d-flex align-items-center justify-content-center choose-level-mobile">
-// 								Porównywanie liczb
-// 							</li>
-// 						</Link>
-// 						<Link style={{ textDecoration: "none" }} to="./un">
-// 							<li className="answer-box-mobile d-flex align-items-center justify-content-center choose-level-mobile">
-// 								Działania z niewiadomą
-// 							</li>
-// 						</Link>
-// 					</ul>
-// 				</div>
-// 			</div>
-// 		</main>
-// 	);
-// }
-
-// export default Dzialy;
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,56 +1,47 @@
 import React from "react";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function Navigation() {
 	return (
 		<nav className="navbar navbar-expand-lg navbar-light py-3 ">
 			<div className="container px-5">
-				<a className="navbar-brand" href="">
-					<div>
-						{/* <img
-							className="appname"
-							src={require("./photo/mis.png")}
-							alt={"Add more descriptive alt"}
-						/> */}
-						<span className="fw-bolder navigation">
-							MATEMATYKA DLA SMYKA
-						</span>
+				<div className="row d-flex align-items-center">
+					<div className="col-8 logo">MATEMATYKA DLA SMYKA</div>
+					<div className="col-4">
+						<button
+							className="navbar-toggler btn-lg"
+							type="button"
+							data-bs-toggle="collapse"
+							data-bs-target="#navbarSupportedContent"
+							aria-controls="navbarSupportedContent"
+							aria-expanded="false"
+							aria-label="Toggle navigation"
+						>
+							<span className="navbar-toggler-icon color-span"></span>
+						</button>
 					</div>
-				</a>
-
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
-				>
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div
-					className="collapse navbar-collapse"
-					id="navbarSupportedContent"
-				>
-					<ul className="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
-						<li className="nav-item">
-							<a className="nav-link navigation" href="numlearn">
-								LICZBY
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link navigation" href="sign">
-								ZNAKI MATEMATYCZNE
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link navigation" href="./">
-								DZIAŁY
-							</a>
-						</li>
-					</ul>
 				</div>
+			</div>
+			<div
+				className="collapse navbar-collapse"
+				id="navbarSupportedContent"
+			>
+				<ul className="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
+					<li className="nav-item">
+						<a className="nav-link navigation" href="./">
+							DZIAŁY
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link navigation" href="numlearn">
+							LICZBY
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link navigation" href="sign">
+							ZNAKI MATEMATYCZNE
+						</a>
+					</li>
+				</ul>
 			</div>
 		</nav>
 	);
