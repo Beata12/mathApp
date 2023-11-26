306a78408011c51c3e1535df272c9c1350c4514f
Beata12 <beata.marek.94@gmail.com>
2023-11-26 10:41:20 +0100

    sound for dzialy

--- src/Components/Dzialy.js
+++ src/Components/Dzialy.js
@@ -1,29 +1,53 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import sound from "../audio/poznajemyLiczby.mp3";
+import numbers from "../audio/poznajemyLiczby.mp3";
+import signs from "../audio/znaki.mp3";
+import add from "../audio/dodawanie.mp3";
+import sub from "../audio/odejmowanie.mp3";
+import comp from "../audio/porownywanie.mp3";
+import ukn from "../audio/niewiadoma.mp3";
 
 function Dzialy() {
 	const [isButtonDisabled, setButtonDisabled] = useState(false);
 
-	function play() {
+	function play(audioFile) {
 		if (!isButtonDisabled) {
-			const audio = new Audio(sound);
+			const audio = new Audio(audioFile);
 			audio.play();
-		}
-	}
-
-	function handleMouseOver() {
-		if (!isButtonDisabled) {
-			play();
-
-			// Zablokuj przycisk na 2no sekund
-			setButtonDisabled(true);
-
-			// Ustaw timer na 2 sekund, po którym odblokujesz przycisk
 			setTimeout(() => {
 				setButtonDisabled(false);
 			}, 2000);
 		}
+	}
+
+	function handleMouseOverNumbers() {
+		play(numbers);
+		setButtonDisabled(true);
+	}
+
+	function handleMouseOverSigns() {
+		play(signs);
+		setButtonDisabled(true);
+	}
+
+	function handleMouseOverAdd() {
+		play(add);
+		setButtonDisabled(true);
+	}
+
+	function handleMouseOverSub() {
+		play(sub);
+		setButtonDisabled(true);
+	}
+
+	function handleMouseOverComp() {
+		play(comp);
+		setButtonDisabled(true);
+	}
+
+	function handleMouseOverUkn() {
+		play(ukn);
+		setButtonDisabled(true);
 	}
 
 	return (
@@ -39,8 +63,7 @@ function Dzialy() {
 								<li className="list-desktop board-desktop hover-menu">
 									<button
 										className="btn-desktop"
-										onClick={play}
-										onMouseOver={handleMouseOver}
+										onMouseOver={handleMouseOverNumbers}
 										disabled={isButtonDisabled}
 									>
 										Poznajemy liczby
@@ -52,36 +75,67 @@ function Dzialy() {
 								to="./sign"
 							>
 								<li className="list-desktop board-desktop hover-menu">
-									Poznajemy znaki matematyczne
+									<button
+										className="btn-desktop"
+										onMouseOver={handleMouseOverSigns}
+										disabled={isButtonDisabled}
+									>
+										Poznajemy znaki matematyczne
+									</button>
 								</li>
 							</Link>
 							<Link style={{ textDecoration: "none" }} to="./add">
 								<li className="list-desktop board-desktop hover-menu">
-									Uczymy się dodawać
+									<button
+										className="btn-desktop"
+										onMouseOver={handleMouseOverAdd}
+										disabled={isButtonDisabled}
+									>
+										Uczymy się dodawać
+									</button>
 								</li>
-							</Link>{" "}
+							</Link>
 							<Link style={{ textDecoration: "none" }} to="./sub">
 								<li className="list-desktop board-desktop hover-menu">
-									Uczymy się odejmować
+									<button
+										className="btn-desktop"
+										onMouseOver={handleMouseOverSub}
+										disabled={isButtonDisabled}
+									>
+										Uczymy się odejmować
+									</button>
 								</li>
-							</Link>{" "}
+							</Link>
 							<Link
 								style={{ textDecoration: "none" }}
 								to="./comp"
 							>
 								<li className="list-desktop board-desktop hover-menu">
-									Porównywanie liczb
+									<button
+										className="btn-desktop"
+										onMouseOver={handleMouseOverComp}
+										disabled={isButtonDisabled}
+									>
+										Porównywanie liczb
+									</button>
 								</li>
 							</Link>
 							<Link style={{ textDecoration: "none" }} to="./un">
 								<li className="list-desktop board-desktop hover-menu">
-									Działania z niewiadomą
+									<button
+										className="btn-desktop"
+										onMouseOver={handleMouseOverUkn}
+										disabled={isButtonDisabled}
+									>
+										Działania z niewiadomą
+									</button>
 								</li>
 							</Link>
 						</ul>
 					</div>
 				</div>
 			</div>
+
 			<div className="dzialy-mobile">
 				<div className="d-flex justify-content-center align-items-center">
 					<ul className="text-center main-mobile">
